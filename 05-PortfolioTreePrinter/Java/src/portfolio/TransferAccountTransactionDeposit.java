ebb06f6101886757fccb2f9978f01568b89bd999
JorgeDee <31420888+JorgeDee@users.noreply.github.com>
2017-09-28 09:40:18 -0300

    Create TransferAccountTransactionDeposit.java

---
+++ 05-PortfolioTreePrinter/Java/src/portfolio/TransferAccountTransactionDeposit.java
@@ -0,0 +1,30 @@
+package portfolio;
+
+public class TransferAccountTransactionDeposit extends TransferLeg{
+
+	private double value;
+	private Transfer trans;
+	
+	
+	public TransferAccountTransactionDeposit(double value, Transfer trans) {
+		this.value = value;
+		this.trans = trans;
+	}
+	
+	@Override
+	public double value() {
+		return value;
+
+	}
+
+	@Override
+	public Transfer transfer() {
+		return trans;
+	}
+	
+	public double sumar() {
+		return value;
+	}
+	
+
+}
