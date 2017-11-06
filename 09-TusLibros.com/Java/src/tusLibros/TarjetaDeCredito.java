dd248783dde9ea6c4d0a60167428fbd7584dab83
noeliaserapio <noeliaserapio@gmail.com>
2017-11-05 22:52:55 -0300

    tarjeta

--- 09-TusLibros.com/Java/src/tusLibros/TarjetaDeCredito.java
+++ 09-TusLibros.com/Java/src/tusLibros/TarjetaDeCredito.java
@@ -15,8 +15,6 @@ public class TarjetaDeCredito {
 	public static final String ERROR_TARJETA_DEBE_TENER_16_CARACTERES = "La tarjeta debe tener 16 caracteres";
 	public static final String ERROR_TARJETA_DEBEN_SER_DIGITOS = "La tarjeta debe tener todos dígitos";
 	public static final String ERROR_DUENIO_FORMATO_INVALIDO = "El dueño de la tarjeta debe comenzar con letras mayúsculas y solo puede contener letras mayúsculas y espacios.";
-	public static final int MAXIMA_CANTIDAD_CARACTERES_DUENIO_TARJETA = 30;
-
 	
 	public TarjetaDeCredito(String numeroTarjeta, YearMonth fechaExpiracion,String nombreDuenio){
 		validarTarjeta(numeroTarjeta, nombreDuenio);
@@ -36,30 +34,8 @@ public class TarjetaDeCredito {
 	}
 
 	public boolean estasVencidaAEstaFecha(LocalDate fecha){
-		int anio = fecha.getYear();
-		Month mes = fecha.getMonth();
-		YearMonth.of(anio, mes);
 		return fechaExpiracion.isBefore(YearMonth.of(fecha.getYear(), fecha.getMonth()));
 	}
 		
-	private boolean esValida(double cantidadDeTransaccion){
-		String[] transacionSplit = new Double(cantidadDeTransaccion).toString().split(".");
-		boolean esValidaCantTransaccion = true;
-		if(transacionSplit.length == 2){
-			esValidaCantTransaccion = (transacionSplit[1].length() <=2) && (transacionSplit[0].length() <=15);
-		}else{
-			esValidaCantTransaccion = (transacionSplit[1].length() <=15);
-		}
-		
-		if(nombreDuenio.length()>MAXIMA_CANTIDAD_CARACTERES_DUENIO_TARJETA){
-			return false;
-		}else if(numeroTarjeta.length() != 16){
-			return false;
-		}else if(!esValidaCantTransaccion){
-			return false;
-		}
-		return true;
-	}
-
 }
 
